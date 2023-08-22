7ff64b8223c3f0e46a35160671168d087df54ba6
SoniaNavale <sonia.navale@gmail.com>
2023-08-22 20:17:04 +0530

    initial commit

--- validString1.js
+++ validString1.js
@@ -1,34 +1,43 @@
-let input = "{}({)}";
+let input = "(])";
 // let input = "([{}]{()}[{}])";
 let inputSplit = input.split("");
 let inputModified = [];
 let stack = [];
 let flag = 0;
+let mapping = {
+  "}": "{",
+  ")": "(",
+  "]": "[",
+};
 
 function strToArray(inputSplit) {
   for (i = 0; i < inputSplit.length; i++) {
     inputModified.push(inputSplit[i]);
   }
+  console.log(inputModified);
 }
 
 function validStr(inputModified) {
-  for (i = inputModified.length; i > 0; i--) {
+  for (i = inputModified.length - 1; i >= 0; i--) {
     inputPopped = inputModified.pop();
 
-    if (inputPopped == "]" || inputPopped == "}" || inputPopped == ")") {
+    if (inputPopped === "]" || inputPopped === "}" || inputPopped === ")") {
       stack.push(inputPopped);
     } else {
       stackPopped = stack.pop();
-      if (inputPopped == stackPopped) {
-        flag = 1;
-      } else {
+
+      // Check if the popped stack element matches the current input character's corresponding bracket
+      if (mapping[stackPopped] !== inputPopped) {
         console.log("invalid string");
+
         break;
+      } else {
+        flag = 1;
       }
     }
   }
   if (flag == 1) {
-    console.log("Valid string");
+    console.log("ValidString");
   }
 }
 
